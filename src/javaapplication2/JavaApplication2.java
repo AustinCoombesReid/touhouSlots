1e08d643cbcc025dff084ab914d618a3e2104391
AustinCoombesReid <austin4@live.ca>
2015-02-05 10:45:00 -0500

    Full Project added

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+
+/**
+ *
+ * @author c0628439
+ */
+public class JavaApplication2 extends NewJFrame {
+  
+    
+    public void main() {
+       
+}
+}
